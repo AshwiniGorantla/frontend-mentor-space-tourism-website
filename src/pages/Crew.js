687d3e1116441a49b7f2a78ba8033e6a493f69e0
ashwini <ashwini92.rymec@gmail.com>
2024-03-26 10:34:32 +1100

    Added some CSS changes to crew and technology pages

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -5,14 +5,20 @@ import data from '../data/data.json';
 function Crew () {
     const [index, setIndex] = useState(0);
 
+    const changeSelection = (event,index) => {
+        event.target.parentNode.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false);
+        event.target.setAttribute("aria-selected", true);
+        setIndex(index);
+    }
+
     return (
         <div className="crew">
             <div className="title"> 02 Meet your crew </div>
             <div className="crew-selector">
-                <span className="dots" onClick={()=>{setIndex(0);}} /> 
-                <span className="dots" onClick={()=>{setIndex(1);}} />
-                <span className="dots" onClick={()=>{setIndex(2);}} /> 
-                <span className="dots" onClick={()=>{setIndex(3);}} />
+                <span className="dots" aria-selected="true" onClick={(event)=>{changeSelection(event,0)}} />
+                <span className="dots" aria-selected="false" onClick={(event)=>{changeSelection(event,1)}} />
+                <span className="dots" aria-selected="false" onClick={(event)=>{changeSelection(event,2)}} />
+                <span className="dots" aria-selected="false" onClick={(event)=>{changeSelection(event,3)}} />
             </div>
             <CrewDetails index={index} data={data.crew} />
         </div>
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -5,13 +5,19 @@ import data from '../data/data.json';
 function Technology () {
     const [index, setIndex] = useState(0);
 
+    const changeSelection = (event,index) => {
+        event.target.parentNode.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false);
+        event.target.setAttribute("aria-selected", true);
+        setIndex(index);
+    }
+
     return (
         <div className="technology">
             <div className="title"> 03 space launch 101 </div>
             <div className="technology-selector">
-                <div className="numberCircle" onClick={()=> {setIndex(0)}}>1</div>
-                <div className="numberCircle" onClick={()=> {setIndex(1)}}>2</div>
-                <div className="numberCircle" onClick={()=> {setIndex(2)}}>3</div>
+                <div className="numberCircle" aria-selected="true" onClick={(event)=>{changeSelection(event,0)}}> 1 </div>
+                <div className="numberCircle" aria-selected="false" onClick={(event)=>{changeSelection(event,1)}}> 2 </div>
+                <div className="numberCircle" aria-selected="false" onClick={(event)=>{changeSelection(event,2)}}> 3 </div>
             </div>
             <TechnologyDetails index={index} data={data.technology} />
         </div>
